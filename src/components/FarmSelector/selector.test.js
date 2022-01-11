afdff55138f814d5c90af8f27fe9efd3c9cf7475
quyentrankhanhha <ha.quyen@thuocsi.vn>
2022-01-12 05:21:19 +0700

    update test

--- src/components/FarmSelector/selector.test.js
+++ src/components/FarmSelector/selector.test.js
@@ -1,52 +1,30 @@
 import { fireEvent, render, screen } from '@testing-library/react'
 import FarmSelector from './index'
 
-describe('FarmSelector', () => {
-  const farms = [
-    {
-      farm_id: '1',
-      location: 'Metsola',
-      name: 'Friman Metsola Collective',
-    },
-  ]
-  test('render component', () => {
-    const { container } = render(
-      <FarmSelector
-        farms={farms}
-        handleOnFarmChange={() => {}}
-        handleOnPeriodChange={() => {}}
-        handleOnSensorChange={() => {}}
-        farmValue={() => {}}
-        periodValue={() => {}}
-        sensorValue={() => {}}
-        handleOnChangeType={() => {}}
-        handleOnFarmChoice={() => {}}
-      />
-    )
-    expect(container).toMatchSnapshot()
-  })
-
-  test('should be have sensor selector like ph sensor when choosing a month period', () => {
-    render(
-      <FarmSelector
-        farms={farms}
-        handleOnFarmChange={() => {}}
-        handleOnPeriodChange={() => {}}
-        handleOnSensorChange={() => {}}
-        farmValue={() => {}}
-        periodValue={() => {}}
-        sensorValue={() => {}}
-        handleOnChangeType={() => {}}
-        handleOnFarmChoice={() => {}}
-      />
-    )
-
-    expect(screen.getByText('A Month')).toBeInTheDocument()
-    expect(screen.getByText('pH')).toBeInTheDocument()
-  })
+const farms = [
+  {
+    farm_id: '1',
+    location: 'Metsola',
+    name: 'Friman Metsola Collective',
+  },
+]
+test('render component', () => {
+  const { container } = render(
+    <FarmSelector
+      farms={farms}
+      handleOnFarmChange={() => {}}
+      handleOnPeriodChange={() => {}}
+      handleOnSensorChange={() => {}}
+      farmValue={() => {}}
+      periodValue={() => {}}
+      sensorValue={() => {}}
+      handleOnChangeType={() => {}}
+      handleOnFarmChoice={() => {}}
+    />
+  )
+  expect(container).toMatchSnapshot()
 })
 
-// highlight-start
 jest.mock('react-select', () => ({ options, value, onChange }) => {
   function handleChange(event) {
     const option = options.find(
@@ -65,12 +43,10 @@ jest.mock('react-select', () => ({ options, value, onChange }) => {
     </select>
   )
 })
-// highlight-end
 
-test('with mock', () => {
+test('should be have sensor selector like ph sensor when choosing a month period', () => {
   render(<FarmSelector />)
   expect(screen.getByText('A Month')).toBeInTheDocument()
-  // highlight-start
   fireEvent.change(screen.getByTestId('select-period'), {
     target: { value: 'month' },
   })
